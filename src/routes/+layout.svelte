7f0e80a8240276b8e2b9d5408aff0190f1cc6bbb
Evans A. Stepanov <evans@stepanov.email>
2023-11-28 20:06:13 -0500

    Add fathom-client dependency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,17 @@
 <script>
 	import '../app.css';
+	import { onMount } from 'svelte';
+	import { browser } from '$app/environment';
+	import { page } from '$app/stores';
+	import * as Fathom from 'fathom-client';
+	import { PUBLIC_FATHOM_SITE_ID } from '$env/static/public';
+
+	onMount(() => {
+		Fathom.load(PUBLIC_FATHOM_SITE_ID);
+	});
+
+	$: $page.url.pathname, browser && Fathom.trackPageview();
+
 </script>
 
 <slot />
